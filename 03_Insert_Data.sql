42d8b68ddf35eb9e7e6a42ed406063fceda4143c
nishu-sharma-001 <sharmanistha95@gmail.com>
2026-08-03 00:10:03 +0530

    Add files via upload

---
+++ 03_Insert_Data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Authors (author_name, country) VALUES
+('J.K. Rowling', 'United Kingdom'),
+('Chetan Bhagat', 'India'),
+('Paulo Coelho', 'Brazil'),
+('George Orwell', 'United Kingdom'),
+('Ruskin Bond', 'India'),
+('Dan Brown', 'United States'),
+('Agatha Christie', 'United Kingdom'),
+('Robin Sharma', 'Canada'),
+('R.K. Narayan', 'India'),
+('James Clear', 'United States');
